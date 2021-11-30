b22dc224ba405c2dd309bbad7f54d6a51908b56a
santiagocorbalan <scorbalan@frba.utn.edu.ar>
2021-11-30 20:14:25 -0300

    Validacion de formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,30 +1,42 @@
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-  });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    
-    var nombre = document.getElementById('nombres').value;
-    
-    if(usuario.length == 0) {
-      alert('No escribió nada en el campo nombre');
-      return;
-    }
-    
-    var apellidos = document.getElementById('apellidos').value;
-    if(usuario.length == 0) {
-        alert('No escribió nada en el campo apellido');
-        return;
-    }
-
-    var correo = document.getElementById('correo').value;
-    if (correo.length < 6) {
-      alert('No escribió nada en el campo correo');
-      return;
-    }
-
-    this.submit();
+function enviarFormulario(){
+  var form = document.form; 
+  if(form.nombre.value==0){
+    alert("El campo Nombre está vacio");
+    form.nombre.value="";
+    form.nombre.focus();
+    return false;
   }
+  if(form.apellido.value==0){
+    alert("El campo Apellido está vacio");
+    form.apellido.value="";
+    form.apellido.focus();
+    return false;}
+
+  if(form.correo.value==0){
+      alert("El campo Correo está vacio");
+      form.correo.value="";
+      form.correo.focus();
+      return false;
+      }
+  
+  if (form.mensaje.value ==0)
+{
+  alert("El campo de mensaje está vacio");
+  form.mensaje.value = "";
+  form.mensaje.focus();
+  return false;
+}
+
+if(form.mensaje.value == 0 && form.correo.value == 0 && form.apellido.value ==0 && form.nombre.value==0){
+  alert("Por favor complete los campos");
+}
+
+alert("Datos enviados con exito");
+form.submit();
+}
+
+
+/* var form = document.getElementById('formulario');
+form.addEventListener('submit', function(evt){
+    console.log
+}) */
